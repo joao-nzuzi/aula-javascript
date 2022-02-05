fed6cc08c0184206cb5d2898c105a27aa3bb67bd
joao-nzuzi <joaonzuzi21@gmail.com>
2022-02-05 22:49:05 +0100

    Primeira Aula de Javascript

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var nome = "João Nzuzi";
+var idade = 29;
+var idade2 = 29;
+alert(nome +" tem "+ idade+ " anos");
+console.log(nome);
+console.log(idade + idade2);
+console.log(nome.toLowerCase);
+console.log(nome.toUpperCase);
