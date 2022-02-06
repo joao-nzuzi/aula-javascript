f9b8609114c959e10e1993813c34cade5d1ecbb7
joao-nzuzi <joaonzuzi21@gmail.com>
2022-02-06 21:24:47 +0100

    Segunda aula de introdução ao Javascritp. Conteúdo estudado -> listas, dicionário e lista de dicionários

--- js/main.js
+++ js/main.js
@@ -6,3 +6,21 @@ console.log(nome);
 console.log(idade + idade2);
 console.log(nome.toLowerCase);
 console.log(nome.toUpperCase);
+
+var lista = ["Maça", "Pêra", "Laranja"];
+lista.push("Uva");
+console.log(lista);
+lista.pop();
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));
+
+var dicionarioFruto = {nome: "Maça", cor: "Vermelha"};
+console.log(dicionarioFruto);
+console.log(dicionarioFruto.nome);
+
+var dicionarioFrutos = [{nome: "Maça", cor: "Vermelha"}, {nome: "Uva", cor: "Roxa"}];
+console.log(dicionarioFrutos);
+console.log(dicionarioFrutos[1].nome);
