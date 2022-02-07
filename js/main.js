8c6d4b3fefaafe8b02f2a8a6f2ae02d435b51218
joao-nzuzi <joaonzuzi21@gmail.com>
2022-02-07 16:23:49 +0100

    Adição do conteúdo da 5ª aula

--- js/main.js
+++ js/main.js
@@ -28,13 +28,13 @@ console.log(dicionarioFrutos);
 console.log(dicionarioFrutos[1].nome);
 
 //3ª aula
-var idade3 = prompt("Qual a sua idade?");
+// var idade3 = prompt("Qual a sua idade?");
 
-if(idade3 >= 18){
-    alert("Maior de idade");
-}else{
-    alert("Menor de idade")
-}
+// if(idade3 >= 18){
+//     alert("Maior de idade");
+// }else{
+//     alert("Menor de idade")
+// }
 
 var count = 0;
 while(count <= 5){
@@ -71,3 +71,29 @@ function validaIdade(idade){
 
 var idade = prompt("Qual a sua idade?")
 console.log(validaIdade(idade))
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    console.log(document.getElementById("agradecimento"))
+}
+
+function redirecionar(){
+    window.open("https://abola.pt/"); // abre outra aba
+    // window.location.href  = "https://abola.pt/"; //Bre na mesma janela
+}
+
+function trocarTexto(elemento){
+    elemento.innerHTML = "Obrigado por passar o rato!";
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Trocar texto";
+}
+
+function load(){
+    alert("Página carregada");
+}
+
+function change(elemento){
+    console.log(elemento.value)
+}
