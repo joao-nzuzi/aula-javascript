bd5da42f97d89e79406b5d8086b81593b7b23fad
joao-nzuzi <joaonzuzi21@gmail.com>
2022-02-07 15:29:02 +0100

    3ª aula de javascript - funções

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
+//1ª aula
 var nome = "João Nzuzi";
 var idade = 29;
 var idade2 = 29;
-alert(nome +" tem "+ idade+ " anos");
+// alert(nome +" tem "+ idade+ " anos");
 console.log(nome);
 console.log(idade + idade2);
 console.log(nome.toLowerCase);
 console.log(nome.toUpperCase);
 
+//2ª aula
 var lista = ["Maça", "Pêra", "Laranja"];
 lista.push("Uva");
 console.log(lista);
@@ -24,3 +26,48 @@ console.log(dicionarioFruto.nome);
 var dicionarioFrutos = [{nome: "Maça", cor: "Vermelha"}, {nome: "Uva", cor: "Roxa"}];
 console.log(dicionarioFrutos);
 console.log(dicionarioFrutos[1].nome);
+
+//3ª aula
+var idade3 = prompt("Qual a sua idade?");
+
+if(idade3 >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade")
+}
+
+var count = 0;
+while(count <= 5){
+    console.log(count);
+    count ++;
+}
+
+var conta;
+for(conta = 0; conta <= 5; conta++){
+    console.log(conta);
+}
+
+var data = new Date();
+console.log(data);
+console.log(data.getDate);
+console.log(data.getMonth + 1);
+
+function soma(numeroUm, numeroDois){
+    return numeroUm + numeroDois;
+}
+
+var numeroUm = prompt("Informa o primeiro número");
+var numeroDois = prompt("Informa o segundo número");
+
+console.log(soma(numeroUm, numeroDois));
+
+function validaIdade(idade){
+    if(idade >= 18){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+var idade = prompt("Qual a sua idade?")
+console.log(validaIdade(idade))
